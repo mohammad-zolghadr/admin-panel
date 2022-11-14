125185d2b99a6225d9dc64dfafda53d0166d0181
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2022-11-15 02:50:40 +0330

    Tailwind Config - Add Fonts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="font-nf h-screen bg-gray-100 flex justify-center items-center">
+      <h1 className="text-md font-bold bg-gray-300 p-10 rounded-lg text-gray-700">
+        Hello world! | سلام دنیا
+      </h1>
+    </div>
+  );
 };
 
 export default App;
