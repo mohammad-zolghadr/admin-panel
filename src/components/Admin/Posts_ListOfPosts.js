2163a90f0785a3ebc3d17aef2f56362dd716be5c
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2022-11-17 12:49:48 +0330

    Posts Component Control What Component Must To Shown-2

---
+++ src/components/Admin/Posts_ListOfPosts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostsListOfPosts = () => {
+  return <div>List of Posts</div>;
+};
+
+export default PostsListOfPosts;
---
+++ src/components/Admin/Posts_NewContent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostsNewContent = () => {
+  return <div>New Content</div>;
+};
+
+export default PostsNewContent;
