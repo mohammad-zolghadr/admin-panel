7b413b712425cc0a0b7be740714c486048b2a1d4
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2022-11-17 21:59:27 +0330

    change PostsListBtn and NewContentBtn style in Posts Component when click on this

--- src/components/Admin/Posts.js
+++ src/components/Admin/Posts.js
@@ -4,6 +4,11 @@ import React, { useEffect, useState } from "react";
 import Posts_NewContent from "./Posts_NewContent";
 import Posts_ListOfPosts from "./Posts_ListOfPosts";
 
+const classActiveButton =
+  "primary-btn shadow-md shadow-purple-300 font-bf md:text-sm text-xs py-2 px-6 md:mx-4 mx-2 mhover";
+const classDeactiveBtn =
+  "secondary-btn shadow-md shadow-purple-300 font-bf md:text-sm text-xs py-2 px-6 md:mx-4 mx-2 mhover";
+
 const Posts = () => {
   const [whatComponentMustShow, setWhatComponentMustShow] =
     useState("NewContent");
@@ -18,14 +23,22 @@ const Posts = () => {
         <button
           name="NewContent"
           onClick={showComponentControl}
-          className="primary-btn shadow-md shadow-purple-300 font-bf md:text-sm text-xs py-2 px-6 md:mx-4 mx-2 mhover"
+          className={
+            whatComponentMustShow === "NewContent"
+              ? classActiveButton
+              : classDeactiveBtn
+          }
         >
           نوشتن مقاله جدید
         </button>
         <button
           name="ListOfPosts"
           onClick={showComponentControl}
-          className="secondary-btn shadow-md shadow-purple-300 font-bf md:text-sm text-xs py-2 px-6 md:mx-4 mx-2 mhover"
+          className={
+            whatComponentMustShow === "ListOfPosts"
+              ? classActiveButton
+              : classDeactiveBtn
+          }
         >
           لیست همه مقاله ها
         </button>
