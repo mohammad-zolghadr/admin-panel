8a5786fc271bb8e35f26b3f75273d964164c4a9c
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2022-11-19 12:41:35 +0330

    Posts_ListOfPosts Table Completed

--- src/components/Admin/Navbar.js
+++ src/components/Admin/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link, Outlet, useLocation } from "react-router-dom";
 
+import profileIco from "../../assets/images/profile.svg";
+
 const RULES = { main: "MAIN_ADMIN", test: "TEST_ADMIN" };
 const changeRoute = (routeUrl) => {
   const fillActiveClass = "fill-purple-500";
@@ -121,17 +123,7 @@ const Navbar = () => {
         <div className="fbetween w-full px-6">
           <div className="fcenter gap-x-4 ">
             <span className="fcenter w-14 h-14 p-2 bg-gray-400 rounded-full">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                className="fill-white w-full"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <img src={profileIco} />
             </span>
             <div className="fstart items-start flex-col  gap-y-1">
               <p className="font-bold text-sm text-gray-600">{data.name}</p>
--- src/components/Admin/Posts_ListOfPosts.js
+++ src/components/Admin/Posts_ListOfPosts.js
@@ -1,7 +1,121 @@
 import React from "react";
 
+// Icons
+import pic from "../../assets/images/profile.svg";
+import trashIco from "../../assets/images/trash.svg";
+import editIco from "../../assets/images/edit.svg";
+
 const PostsListOfPosts = () => {
-  return <div>List of Posts</div>;
+  const tableData = [
+    {
+      id: 1,
+      title: "عنوان یک مقاله را اینجا می نویسیم",
+      image: pic,
+      status: "active",
+      date: "1401/08/28",
+    },
+    {
+      id: 2,
+      title: "زندگی را دوست بداریم",
+      image: pic,
+      status: "active",
+      date: "1396/06/28",
+    },
+    {
+      id: 3,
+      title: "آموزش برنامه نویسی فرانت اند",
+      image: pic,
+      status: "deactive",
+      date: "1401/10/22",
+    },
+    {
+      id: 4,
+      title: "یک آموزش جدید میخوام بسازم براتون",
+      image: pic,
+      status: "active",
+      date: "1403/04/02",
+    },
+  ];
+
+  return (
+    <div>
+      <h3 className="font-bf text-gray-700">لیست مقاله ها</h3>
+
+      <div>{/* Search - Filter */}</div>
+
+      <div className="border border-gray-300 rounded-lg my-4">
+        <table className="leading-normal w-full">
+          <thead>
+            <tr>
+              <th
+                scope="col"
+                className="font-normal border-b border-gray-300 text-right text-gray-500 text-sm px-4 py-3"
+              >
+                عنوان
+              </th>
+              <th
+                scope="col"
+                className="font-normal border-b border-gray-300 text-center text-gray-500 text-sm px-4 py-3"
+              >
+                وضعیت
+              </th>
+              <th
+                scope="col"
+                className="font-normal border-b border-gray-300 text-center text-gray-500 text-sm px-4 py-3"
+              >
+                ایجاد شده در
+              </th>
+              <th
+                scope="col"
+                className="font-normal border-b border-gray-300 text-center text-gray-500 text-sm px-4 py-3"
+              >
+                اقدامات بیشتر
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {tableData.map((e) => {
+              return (
+                <tr key={e.id} className="border-b border-gray-200">
+                  <td className="py-4 px-2">
+                    <div className="fcenter justify-start gap-2">
+                      <img
+                        src={e.image}
+                        className="hidden md:inline-block w-10 h-10 p-2 bg-gray-300 rounded-full"
+                      ></img>
+                      <p className="text-xs font-bf text-gray-500 ">
+                        {e.title}
+                      </p>
+                    </div>
+                  </td>
+                  <td>
+                    <div className="fcenter">
+                      {e.status === "active" ? (
+                        <span className="mactive">فعال</span>
+                      ) : (
+                        <span className="mdarft">پیش نویس</span>
+                      )}
+                    </div>
+                  </td>
+                  <td>
+                    <div className="fcenter">
+                      <span className="text-xs text-gray-400">{e.date}</span>
+                    </div>
+                  </td>
+                  <td>
+                    <div className="fcenter gap-2">
+                      <img src={editIco} className="w-5 mhover" />
+                      <img src={trashIco} className="w-5 mhover" />
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default PostsListOfPosts;
