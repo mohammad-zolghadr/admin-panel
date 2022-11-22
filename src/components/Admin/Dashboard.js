8110288caa639502b9023acfa1c4e2e14f3f8911
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2022-11-23 01:34:31 +0330

    Dashboard Page Building

--- src/components/Admin/Dashboard.js
+++ src/components/Admin/Dashboard.js
@@ -3,8 +3,8 @@ import React from "react";
 const Dashboard = () => {
   return (
     <div className="font-nf h-screen bg-gray-100 flex justify-center items-center">
-      <h1 className="text-md font-bold bg-gray-300 p-10 rounded-lg text-gray-700">
-        Dashboard
+      <h1 className="text-md font-bold  text-gray-700">
+        در حال طراحی و پیاده سازی داشبورد...
       </h1>
     </div>
   );
