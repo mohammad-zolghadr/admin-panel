44c68a7b3341fc737d905b8a8392e34ce434652c
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2022-11-29 21:28:24 +0330

    Dashboard : Message Section - Part 01

--- src/components/Admin/Dashboard.js
+++ src/components/Admin/Dashboard.js
@@ -7,6 +7,9 @@ import AreaChart from "./AreaChart";
 import userIco from "../../assets/images/users.svg";
 import messageIco from "../../assets/images/message.svg";
 import postIco from "../../assets/images/posts.svg";
+import trashIco from "../../assets/images/trash_white.svg";
+import replyIco from "../../assets/images/reply_arrow.svg";
+import arrowIco from "../../assets/images/arrow.svg";
 
 // Chart Data
 const labels = [
@@ -43,6 +46,35 @@ const postsChartData = {
   ],
 };
 
+// Table Data
+const tableData = [
+  {
+    id: 1,
+    message: "متن پیام به صورت خلاصه شده...",
+    email: "exampla@gmail.com",
+    date: "1401/08/28",
+  },
+  {
+    id: 2,
+    message: "فلان قسمت مشکل داره داداش",
+    email: "exampla@gmail.com",
+    date: "1396/06/28",
+  },
+  {
+    id: 3,
+    message:
+      "این قسمت فقط برای پیام مستقیم به ادمین هست نه کامنت گذاشتن زیر مقاله ها و پست ها",
+    email: "exampla@gmail.com",
+    date: "1401/10/22",
+  },
+  {
+    id: 4,
+    message: "یه پیشنهاد جدید دارم برای شما",
+    email: "exampla@gmail.com",
+    date: "1403/04/02",
+  },
+];
+
 // Calculate numbers in array
 const calculateNumbers = (array) => {
   const calc = array.reduce((a, b) => a + b);
@@ -62,7 +94,77 @@ const Dashboard = () => {
     if (showWhichChart === "week") return <AreaChart data={weekChartData} />;
     else if (showWhichChart === "post")
       return <AreaChart data={postsChartData} />;
-    else if (showWhichChart === "message") return <h2>لیست پیام ها</h2>;
+    else if (showWhichChart === "message")
+      return (
+        <div className="w-full border border-gray-300 rounded-lg mt-32 md:mt-0">
+          <table className="w-full leading-normal">
+            <thead>
+              <tr>
+                <th scope="col" className={`${tableHeadItemStyle} text-start`}>
+                  متن پیام
+                </th>
+                <th scope="col" className={tableHeadItemStyle}>
+                  ارسال شده توسط
+                </th>
+                <th scope="col" className={tableHeadItemStyle}>
+                  ارسال شده در
+                </th>
+                <th scope="col" className={tableHeadItemStyle}>
+                  اقدامات بیشتر
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableData.map((e) => {
+                return (
+                  <tr key={e.id} className="w-full border-b border-gray-200">
+                    <td className=" py-4 px-2">
+                      <div className="fcenter justify-start gap-2">
+                        <p className="text-xs text-gray-500 ">
+                          {e.message.substring(0, 50)}...
+                        </p>
+                      </div>
+                    </td>
+                    <td className="">
+                      <div className="fcenter">
+                        <span className="text-xs text-gray-400">{e.email}</span>
+                      </div>
+                    </td>
+                    <td>
+                      <div className="fcenter">
+                        <span className="text-xs text-gray-400">{e.date}</span>
+                      </div>
+                    </td>
+                    <td>
+                      <div className="fcenter gap-2">
+                        <img
+                          src={replyIco}
+                          className="w-6 bg-green-700 rounded-full p-1 mhover"
+                        />
+                        <img
+                          src={trashIco}
+                          className="w-6 bg-red-600 rounded-full p-1 mhover"
+                        />
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          {/* Pagination */}
+          <div className="fcenter py-3">
+            <div className="fcenter text-gray-400 border rounded-md overflow-hidden">
+              <button className="bg-gray-200 px-2 pr-3 border-l">1</button>
+              <button className="hover:bg-gray-100 px-2 border-l">2</button>
+              <button className="hover:bg-gray-100 px-2 border-l">3</button>
+              <button className="fcenter w-6 h-6 hover:bg-gray-100">
+                <img src={arrowIco} className="w-3 rotate-90" />
+              </button>
+            </div>
+          </div>
+        </div>
+      );
   };
 
   return (
@@ -120,3 +222,5 @@ const classTopHeader =
   "fcenter gap-2 md:gap-6 h-20 lg:h-24 flex-grow rounded-lg shadow-lg px-4 mhover";
 const classTopHeaderCounter = "text-sm lg:text-base text-white";
 const classTopHeaderTitle = "text-xs lg:text-sm text-white";
+const tableHeadItemStyle =
+  "font-normal border-b border-gray-300 text-center text-gray-500 text-sm px-4 py-3";
