9e404fafa8b5f7d266f7e66624f50b27bfe44859
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2022-12-07 11:42:04 +0330

    Create Loading Component

---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Style
+import style from "../sass/Loading.scss";
+
+const Loading = () => {
+  return <div className="loadingCircle animate-spin"></div>;
+};
+
+export default Loading;
