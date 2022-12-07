9cc6807956dc162f30fe4065a7c49157e2d7aa53
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2022-12-07 11:56:45 +0330

    TechnologyAddOrEdit Dialog : use Loading Component

--- src/components/Admin/dialog-editInfo/TechnologyAddOrEdit.js
+++ src/components/Admin/dialog-editInfo/TechnologyAddOrEdit.js
@@ -9,8 +9,12 @@ import {
 } from "../redux/redux-info/infoActions";
 import { uploadImage } from "../../../fakeBackendFuctions";
 
+// Components
+import Loading from "../../Loading";
+
 const TechnologyAddOrEdit = (props) => {
   const { dialogFunction, dialogData } = props.data;
+  const [isLoading, setIsLoading] = useState(false);
   const setData = () => {
     if (dialogData)
       return {
@@ -29,27 +33,28 @@ const TechnologyAddOrEdit = (props) => {
   const dispatch = useDispatch();
 
   const clickHandler = (e) => {
-    dialogFunction({ isShow: false });
-
     if (e.target.name === "confirm") {
+      setIsLoading(true);
       if (!dialogData)
-        uploadImage(inputFieldValue.link).then((imageLink) =>
+        uploadImage(inputFieldValue.link).then((imageLink) => {
+          dialogFunction({ isShow: false });
           dispatch(
             addItemToListOfTechnologies({ ...inputFieldValue, link: imageLink })
-          )
-        );
+          );
+        });
       else {
-        uploadImage(inputFieldValue.link).then((imageLink) =>
+        uploadImage(inputFieldValue.link).then((imageLink) => {
+          dialogFunction({ isShow: false });
           dispatch(
             editItemOfTechnologies({
               ...inputFieldValue,
               link: imageLink,
               id: dialogData.id,
             })
-          )
-        );
+          );
+        });
       }
-    }
+    } else if (!isLoading) dialogFunction({ isShow: false });
   };
 
   return (
@@ -58,69 +63,75 @@ const TechnologyAddOrEdit = (props) => {
         onClick={clickHandler}
         className="block absolute w-full h-screen z-20 bg-black opacity-80"
       ></span>
-      <div className="bg-white px-8 py-6 rounded-lg fcenter flex-col gap-4 z-20">
-        <div className="fcenter gap-2">
+      {isLoading ? (
+        <div className="w-12 h-12 z-30">
+          <Loading />
+        </div>
+      ) : (
+        <div className="bg-white px-8 py-6 rounded-lg fcenter flex-col gap-4 z-20">
+          <div className="fcenter gap-2">
+            <input
+              value={inputFieldValue.name}
+              className="minput w-56 shadow-md py-3 ph text-xs"
+              placeholder="اسم تخصص را اینجا وارد کنید"
+              type="text"
+              onChange={(e) => {
+                setInputFieldValue({
+                  ...inputFieldValue,
+                  name: e.target.value,
+                });
+              }}
+            />
+            <div className="bg-gray-100 py-1 px-2 rounded-md shadow-md">
+              <label className="fcenter" forhtml="choosePicture">
+                <span className="bg-gray-200 font-bf text-xs text-gray-400 px-3 py-2 rounded-md mhover ">
+                  انتخاب عکس
+                </span>
+                <input
+                  id="choosePicture"
+                  type="file"
+                  className="hidden"
+                  accept="image/jpeg , image/png , .svg"
+                  onChange={(e) => {
+                    setInputFieldValue({
+                      ...inputFieldValue,
+                      link: e.target.files[0],
+                    });
+                  }}
+                />
+              </label>
+            </div>
+          </div>
           <input
-            value={inputFieldValue.name}
-            className="minput w-56 shadow-md py-3 ph text-xs"
-            placeholder="اسم تخصص را اینجا وارد کنید"
+            value={inputFieldValue.tech}
+            className="minput w-full shadow-md py-3 ph text-xs"
+            placeholder="اسم تکنولوژی ها را اینجا وارد کنید و با - از هم جدا کنید"
             type="text"
             onChange={(e) => {
               setInputFieldValue({
                 ...inputFieldValue,
-                name: e.target.value,
+                tech: e.target.value,
               });
             }}
           />
-          <div className="bg-gray-100 py-1 px-2 rounded-md shadow-md">
-            <label className="fcenter" forhtml="choosePicture">
-              <span className="bg-gray-200 font-bf text-xs text-gray-400 px-3 py-2 rounded-md mhover ">
-                انتخاب عکس
-              </span>
-              <input
-                id="choosePicture"
-                type="file"
-                className="hidden"
-                accept="image/jpeg , image/png , .svg"
-                onChange={(e) => {
-                  setInputFieldValue({
-                    ...inputFieldValue,
-                    link: e.target.files[0],
-                  });
-                }}
-              />
-            </label>
+          <div className="fcenter gap-2">
+            <button
+              name="cancel"
+              onClick={clickHandler}
+              className="px-6 py-1 rounded-lg font-bf mhover bg-red-300 text-red-700"
+            >
+              لغو
+            </button>
+            <button
+              name="confirm"
+              onClick={clickHandler}
+              className="px-6 py-1 rounded-lg font-bf mhover bg-green-700 text-white"
+            >
+              تایید
+            </button>
           </div>
         </div>
-        <input
-          value={inputFieldValue.tech}
-          className="minput w-full shadow-md py-3 ph text-xs"
-          placeholder="اسم تکنولوژی ها را اینجا وارد کنید و با - از هم جدا کنید"
-          type="text"
-          onChange={(e) => {
-            setInputFieldValue({
-              ...inputFieldValue,
-              tech: e.target.value,
-            });
-          }}
-        />
-        <div className="fcenter gap-2">
-          <button
-            name="cancel"
-            onClick={clickHandler}
-            className="px-6 py-1 rounded-lg font-bf mhover bg-red-300 text-red-700"
-          >
-            لغو
-          </button>
-          <button
-            name="confirm"
-            onClick={clickHandler}
-            className="px-6 py-1 rounded-lg font-bf mhover bg-green-700 text-white"
-          >
-            تایید
-          </button>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
